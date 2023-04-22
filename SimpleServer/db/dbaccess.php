b0873896eee13e0c1e6e10db981f4dcb99f1d79a
Frazerus <e12032554@student.tuwien.ac.at>
2023-04-22 15:41:40 +0200

    AYEEE

---
+++ SimpleServer/db/dbaccess.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "localhost";
+    $user = "bif2webscriptinguser";
+    $password = "bif2021";
+    $database = "bif2webscriptdb";
+    //Datenbankzugriff, dieser User und Datenbank heißt so
+?>
---
+++ SimpleServer/models/appointment.php
@@ -0,0 +1,18 @@
+<?php
+class Appointment {
+    public $title;
+    public $location;
+    public $description;
+    public $duration;
+    public $dates;
+
+    function __construct($title, $location, $description, $duration, $dates) {
+        $this->title = $title;
+        $this->location = $location;
+        $this->description=$description;
+        $this->duration=$duration;
+        $this->dates=$dates;
+
+      }
+}
+?>
